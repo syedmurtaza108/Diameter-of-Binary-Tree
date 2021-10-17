98f0365995ea0e84a22465730f2e033f5002f6d8
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-17 21:36:53 +0500

    Add project files.

---
+++ Diameter of Binary Tree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diameter_of_Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
